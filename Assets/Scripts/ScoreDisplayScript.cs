1407f2bb9b0728dcabaa929d7c2dba91e972e789
Sandy T <hurricanesandytam@gmail.com>
2026-03-14 00:35:06 -0700

    reverting changes, updating RL reward system

--- Assets/Scripts/ScoreDisplayScript.cs
+++ Assets/Scripts/ScoreDisplayScript.cs
@@ -9,35 +9,23 @@ public class ScoreDisplayScript : MonoBehaviour
     private GameObject _digitPrefab;
     [SerializeField]
     private Transform _scoreDisplay;
-    [SerializeField]
-    private Transform _bestScoreDisplay; 
-
     private int _score = 0;
-    private int _bestScore = 0; 
 
     void Start()
     {
         UpdateScoreDisplay();
-        UpdateBestScoreDisplay(); 
     }
-
     public void AddScore()
     {
         _score++;
         UpdateScoreDisplay();
-
-        if (_score > _bestScore)
-        {
-            _bestScore = _score;
-            UpdateBestScoreDisplay();
-        }
     }
 
     public void UpdateScoreDisplay()
     {
-        foreach (Transform child in _scoreDisplay)
+        foreach (Transform chlid in _scoreDisplay)
         {
-            Destroy(child.gameObject);
+            Destroy(chlid.gameObject);
         }
 
         string scoreStr = _score.ToString();
@@ -49,27 +37,9 @@ public class ScoreDisplayScript : MonoBehaviour
         }
     }
 
-    // NEW
-    public void UpdateBestScoreDisplay()
-    {
-        foreach (Transform child in _bestScoreDisplay)
-        {
-            Destroy(child.gameObject);
-        }
-
-        string bestStr = _bestScore.ToString();
-        foreach (char digitChar in bestStr)
-        {
-            int digit = int.Parse(digitChar.ToString());
-            GameObject newDigit = Instantiate(_digitPrefab, _bestScoreDisplay);
-            newDigit.GetComponent<Image>().sprite = _numSprites[digit];
-        }
-    }
-
     public void ResetScore()
     {
         _score = 0;
         UpdateScoreDisplay();
-        // best score intentionally not reset
     }
 }
