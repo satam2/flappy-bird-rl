917d0c42092b55811d658a36ec73cf5d2fbbcfdf
Sandy T <hurricanesandytam@gmail.com>
2026-03-14 01:41:55 -0700

    fixed best score bug

--- flappy_bird/Assets/Scripts/ScoreDisplayScript.cs
+++ flappy_bird/Assets/Scripts/ScoreDisplayScript.cs
@@ -9,23 +9,35 @@ public class ScoreDisplayScript : MonoBehaviour
     private GameObject _digitPrefab;
     [SerializeField]
     private Transform _scoreDisplay;
+    [SerializeField]
+    private Transform _bestScoreDisplay;
+
     private int _score = 0;
+    private int _bestScore = 0;
 
     void Start()
     {
         UpdateScoreDisplay();
+        UpdateBestScoreDisplay();
     }
+
     public void AddScore()
     {
         _score++;
         UpdateScoreDisplay();
+
+        if (_score > _bestScore)
+        {
+            _bestScore = _score;
+            UpdateBestScoreDisplay();
+        }
     }
 
     public void UpdateScoreDisplay()
     {
-        foreach (Transform chlid in _scoreDisplay)
+        foreach (Transform child in _scoreDisplay)
         {
-            Destroy(chlid.gameObject);
+            Destroy(child.gameObject);
         }
 
         string scoreStr = _score.ToString();
@@ -37,9 +49,26 @@ public class ScoreDisplayScript : MonoBehaviour
         }
     }
 
+    public void UpdateBestScoreDisplay()
+    {
+        foreach (Transform child in _bestScoreDisplay)
+        {
+            Destroy(child.gameObject);
+        }
+
+        string bestStr = _bestScore.ToString();
+        foreach (char digitChar in bestStr)
+        {
+            int digit = int.Parse(digitChar.ToString());
+            GameObject newDigit = Instantiate(_digitPrefab, _bestScoreDisplay);
+            newDigit.GetComponent<Image>().sprite = _numSprites[digit];
+        }
+    }
+
     public void ResetScore()
     {
         _score = 0;
         UpdateScoreDisplay();
+        // best score intentionally not reset
     }
 }
